0092e05faae922f64029a009cb55186277d834bd
Mohamed-Dhia <dhiawesalti16@gmail.com>
2019-12-26 19:41:26 +0100

    a7a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,116 +1,90 @@
-module.exports = function (grunt) {
-
+module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     concat: {
-      src: [
-        "app/**/*.js",
-        "lib/*.js",
-        "*.js"
-      ],
-      dest: 'dist/built.js',
+      src: ["app/**/*.js", "lib/*.js", "*.js"],
+      dest: "dist/built.js"
     },
 
     mochaTest: {
       test: {
         options: {
-          reporter: 'spec'
+          reporter: "spec"
         },
-        src: ['test/**/*.js']
+        src: ["test/**/*.js"]
       }
     },
 
     nodemon: {
       dev: {
-        script: 'server.js'
+        script: "server.js"
       }
     },
 
-    uglify: {
-    },
+    uglify: {},
 
     eslint: {
-      target: [
-        "app/**/*.js",
-        "lib/*.js",
-        "*.js"
-      ]
+      target: ["app/**/*.js", "lib/*.js", "*.js"]
     },
 
-    cssmin: {
-    },
+    cssmin: {},
 
     watch: {
       scripts: {
-        files: [
-          'public/client/**/*.js',
-          'public/lib/**/*.js',
-        ],
-        tasks: [
-          'concat',
-          'uglify'
-        ]
+        files: ["public/client/**/*.js", "public/lib/**/*.js"],
+        tasks: ["concat", "uglify"]
       },
       css: {
-        files: 'public/*.css',
-        tasks: ['cssmin']
+        files: "public/*.css",
+        tasks: ["cssmin"]
       }
     },
 
     shell: {
-
       add: {
-        command: 'git add .'
+        command: "git add ."
       },
       commit: {
         command: 'git commit -m "commited by grunt gang"'
       },
       push: {
-        command: 'git push heroku master'
+        command: "git push    master"
       }
-
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-eslint");
+  grunt.loadNpmTasks("grunt-mocha-test");
+  grunt.loadNpmTasks("grunt-shell");
+  grunt.loadNpmTasks("grunt-nodemon");
 
-  grunt.registerTask('server-dev', function (target) {
-    grunt.task.run(['nodemon', 'watch']);
+  grunt.registerTask("server-dev", function(target) {
+    grunt.task.run(["nodemon", "watch"]);
   });
 
   ////////////////////////////////////////////////////
   // Main grunt tasks
   ////////////////////////////////////////////////////
 
-  grunt.registerTask('test', [
-    "eslint",
-    'mochaTest'
-  ]);
+  grunt.registerTask("test", ["eslint", "mochaTest"]);
 
-  grunt.registerTask('build', [
-  ]);
+  grunt.registerTask("build", []);
 
-  grunt.registerTask('upload', function (n) {
-    if (grunt.option('prod')) {
+  grunt.registerTask("upload", function(n) {
+    if (grunt.option("prod")) {
       // add your production server task here
     } else {
-      grunt.task.run(['server-dev']);
+      grunt.task.run(["server-dev"]);
     }
   });
 
-  grunt.registerTask('deploy', [
+  grunt.registerTask("deploy", [
     // add your deploy tasks here
     // grunt.task.run(['concat', 'uglify', 'cssmin', 'shell:prodServer:add', 'shell:prodServer:commit', 'shell:prodServer:push'])
-    grunt.task.run(['shell:add', 'shell:commit', 'shell:push'])
-
-  ])
-
-
+    grunt.task.run(["shell:add", "shell:commit", "shell:push"])
+  ]);
 };
--- db/database.js
+++ db/database.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://maha:Chahed117719@cluster0-irssu.mongodb.net/test?retryWrites=true&w=majority');
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+const db = mongoose.connection;
+mongoose.set("useCreateIndex", true);
+mongoose.Promise = global.Promise;
 
+db.once("open", () => {
+  console.log("Mongoose is live wooohhhho!");
+});
 
+const usersSchema = new Schema({
+  username: String,
+  password: String,
+  timestamps: { type: Date, default: Date.now }
+});
 
 const urlSchema = new Schema({
-  url: String,
+  url: { type: String, unique: true },
   baseURL: String,
   code: String,
   title: String,
-  visits: Number,
+  visits: { type: Number, default: 0 },
   user: usersSchema,
   timestamps: { type: Date, default: Date.now }
 });
 
+var BookShelf = mongoose.model("Bookshelf", urlSchema);
 
-const usersSchema = new Schema({
-  username: { Type: String, unique: true },
-  password: String,
-  timestamps: { type: Date, default: Date.now }
-
-})
-
-
-
-var BookShelf = mongoose.model('Bookshelf', urlSchema)
+module.exports.save = obj => {
+  return BookShelf.create(obj);
+};
